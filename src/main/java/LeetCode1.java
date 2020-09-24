72898b8d18b95224b7d16b0f39f0d1a1c47c5c68
lty <993814548@qq.com>
2020-09-24 19:53:17 +0800

    完成题目8. 字符串转换整数 (atoi) 难度-中等 优先百分之18 完成题目6. Z 字形变换 难度-中等 优先百分之99

--- src/main/java/LeetCode1.java
+++ src/main/java/LeetCode1.java
@@ -1166,7 +1166,159 @@ public class LeetCode1 {
 
     public static void main(String[] args) {
        // outListNode(createListNode(123l));
-       outListNode(addTwoNumbers(createListNode(0),createListNode(567)));
+       //outListNode(addTwoNumbers(createListNode(0),createListNode(567)));
+        StringBuilder a=new StringBuilder("123213123");
+
+
+        System.out.println(myAtoi(" -11919730356x"));
+    }
+
+    /**
+     * 8. 字符串转换整数 (atoi)
+     * 请你来实现一个 atoi 函数，使其能将字符串转换成整数。
+     *
+     * 首先，该函数会根据需要丢弃无用的开头空格字符，直到寻找到第一个非空格的字符为止。接下来的转化规则如下：
+     *
+     * 如果第一个非空字符为正或者负号时，则将该符号与之后面尽可能多的连续数字字符组合起来，形成一个有符号整数。
+     * 假如第一个非空字符是数字，则直接将其与之后连续的数字字符组合起来，形成一个整数。
+     * 该字符串在有效的整数部分之后也可能会存在多余的字符，那么这些字符可以被忽略，它们对函数不应该造成影响。
+     * 注意：假如该字符串中的第一个非空格字符不是一个有效整数字符、字符串为空或字符串仅包含空白字符时，则你的函数不需要进行转换，即无法进行有效转换。
+     *
+     * 在任何情况下，若函数不能进行有效的转换时，请返回 0 。
+     *
+     * 提示：
+     *
+     * 本题中的空白字符只包括空格字符 ' ' 。
+     * 假设我们的环境只能存储 32 位大小的有符号整数，那么其数值范围为 [−231,  231 − 1]。如果数值超过这个范围，请返回  INT_MAX (231 − 1) 或 INT_MIN (−231) 。
+     *
+     *
+     * 示例 1:
+     *
+     * 输入: "42"
+     * 输出: 42
+     * 示例 2:
+     *
+     * 输入: "   -42"
+     * 输出: -42
+     * 解释: 第一个非空白字符为 '-', 它是一个负号。
+     *      我们尽可能将负号与后面所有连续出现的数字组合起来，最后得到 -42 。
+     * 示例 3:
+     *
+     * 输入: "4193 with words"
+     * 输出: 4193
+     * 解释: 转换截止于数字 '3' ，因为它的下一个字符不为数字。
+     * 示例 4:
+     *
+     * 输入: "words and 987"
+     * 输出: 0
+     * 解释: 第一个非空字符是 'w', 但它不是数字或正、负号。
+     *      因此无法执行有效的转换。
+     * 示例 5:
+     *
+     * 输入: "-91283472332"
+     * 输出: -2147483648
+     * 解释: 数字 "-91283472332" 超过 32 位有符号整数范围。
+     *      因此返回 INT_MIN (−231) 。
+     * @param str
+     * @return
+     */
+    public static int myAtoi(String str) {
+        str=str.trim();
+        StringBuilder sbd=new StringBuilder();
+        int count=0;
+        char s=0;
+       for(int i=0;i<str.length();i++){
+           if(!Character.isDigit(str.charAt(i))){
+               if(sbd.length()>0){
+                   break;
+               }
+               if((str.charAt(i)=='-'||str.charAt(i)=='+')&&count==0){
+                  count++;
+                  s=str.charAt(i);
+                  continue;
+               }
+             break;
+           }
+           sbd.append(str.charAt(i));
+       }
+       if(sbd.length()>0){
+           for(int i=0;i<sbd.length();i++){
+               if(sbd.charAt(i)!='0'){
+                  break;
+               }
+               sbd.deleteCharAt(i);
+               i--;
+           }
+       }
+       if(count>0){
+           sbd.insert(0,s);
+       }
+       if(sbd.length()==0||(sbd.length()==1&&count==1)){
+           return 0;
+       }
+       Long lty=Long.parseLong(sbd.length()>11?sbd.substring(0,12):sbd.toString());
+        return lty>Integer.MAX_VALUE?Integer.MAX_VALUE:lty<Integer.MIN_VALUE?Integer.MIN_VALUE:lty.intValue();
+    }
+
+    /**
+     * 6. Z 字形变换
+     * 将一个给定字符串根据给定的行数，以从上往下、从左到右进行 Z 字形排列。
+     *
+     * 比如输入字符串为 "LEETCODEISHIRING" 行数为 3 时，排列如下：
+     *
+     * L   C   I   R
+     * E T O E S I I G
+     * E   D   H   N
+     * 之后，你的输出需要从左往右逐行读取，产生出一个新的字符串，比如："LCIRETOESIIGEDHN"。
+     * LCIRETOESIIGEDHN
+     * LCIRETOESIIGEDHN
+     * 请你实现这个将字符串进行指定行数变换的函数：
+     *
+     * string convert(string s, int numRows);
+     * 示例 1:
+     *
+     * 输入: s = "LEETCODEISHIRING", numRows = 3
+     * 输出: "LCIRETOESIIGEDHN"
+     *
+     * 示例 2:
+     *
+     * 输入: s = "LEETCODEISHIRING", numRows = 4
+     * 输出: "LDREOEIIECIHNTSG"
+     *       LDREOEII
+     * 解释:
+     *
+     * L     D     R
+     * E   O E   I I
+     * E C   I H   N
+     * T     S     G
+     * @param s
+     * @param numRows
+     * @return
+     */
+    public static String convert(String s, int numRows) {
+        StringBuilder sbd=new StringBuilder(s.length());
+        int a=numRows+numRows-2;
+        int count=s.length()/a;
+            count+=s.length()%a!=0?1:0;
+        Boolean isTwo=false;
+            one:
+            for(int i=0;i<numRows;i++){
+                isTwo=i!=0&&i!=numRows-1;
+                for(int y=0;y<count;y++){
+                    int index=y*a+i;
+                    if(index>s.length()-1){
+                        break one;
+                    }
+                    sbd.append(s.charAt(index));
+                    if(isTwo){
+                        if(index+a-i*2>s.length()-1){
+                            continue one;
+                        }
+                        sbd.append(s.charAt(index+a-i*2));
+                    }
+                }
+            }
+        return sbd.toString();
     }
 
     /**
